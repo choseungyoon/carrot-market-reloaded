74f7b15e3c9ac517642b7c59a6b10251ab283747
choseungyoon <cho31250@gmail.com>
2024-05-16 19:40:25 +0900

    Add login page and tailwindcss setting

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={`${inter.className} bg-neutral-900 max-w-sm text-white mx-auto`}
+      >
+        {children}
+      </body>
     </html>
   );
 }
