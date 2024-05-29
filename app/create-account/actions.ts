16ed81f8f4cbe8deedad6508631a209fe5b683b3
choseungyoon <cho31250@gmail.com>
2024-05-29 14:02:12 +0900

    Add custom validation

--- app/create-account/actions.ts
+++ app/create-account/actions.ts
@@ -1,11 +1,30 @@
 "use server"
 import {z} from "zod"
 
+// At least one uppercase letter, one lowercase letter, one number and one special character
+const passwordRegex = new RegExp(
+    /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).+$/
+    );
+
+const checkUsername = (username : string) => !username.includes("potato");
+const checkPassword = ({password,confirmPassword}:{password:string, confirmPassword:string}) => password === confirmPassword
 const formSchema = z.object({
-    username: z.string().min(3).max(10),
-    email : z.string().email(),
-    password: z.string().min(10),
+    username: z
+    .string({
+        invalid_type_error : "Username must be a string!",
+        required_error : "Where is my username??"
+    })
+    .trim()
+    .toLowerCase()
+    .min(3,"Way too short!!")
+    .max(10,"That is too long!!")
+    .refine(checkUsername, "custom error"),
+    email : z.string().email().toLowerCase(),
+    password: z.string().min(10).regex(passwordRegex,"A password must have lowercase, UPPERCASE, a number and special characters"),
     confirmPassword : z.string().min(10),
+}).refine(checkPassword, {
+    message : "Both passwords should be the same!",
+    path : ["confirmPassword"]
 })
 
 export async function createAccount(prevStage: any, FormData: FormData){
@@ -21,7 +40,8 @@ export async function createAccount(prevStage: any, FormData: FormData){
     if(!result.success){
         return result.error.flatten()
     }
+    else{
+        console.log(data)
+    }
 
-
-    console.log(data)
 }
